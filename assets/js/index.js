dac6844fef9e2b90d1b5b4b74e6861d10bf07455
zhangsan <1015635459@qq.com>
2021-04-25 23:57:36 +0800

    提交了2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,5 @@
 $(function() {
 
-
-
-
-
     $('.loginbox_rep').on('click', function() {
         $('.login').hide();
         $('.rep').show();
@@ -14,6 +10,8 @@ $(function() {
         $('.rep').hide();
     })
     var form = layui.form
+
+    var layer = layui.layer
     form.verify({
         pass: [
             /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
@@ -26,35 +24,57 @@ $(function() {
         }
     })
 
-    // $('.rep').submit(function(e) {
-    //     e.preventDefault()
-    //     $.ajax({
-    //         type: 'post',
-    //         url: 'http://ajax.frontend.itheima.net/api/reguser',
-    //         data: {
-    //             username: $('.rep2').val(),
-    //             password: $('.rep1').val(),
+    $('.rep').submit(function(e) {
+        e.preventDefault()
+        $.ajax({
+            type: 'post',
+            url: 'http://api-breakingnews-web.itheima.net/api/reguser',
+            data: {
+                username: $('.rep2').val(),
+                password: $('.rep1').val(),
 
-    //         },
-    //         success: function(res) {
-    //             console.log(res);
+            },
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('失败');
+                } else {
+                    layer.msg('登陆成功');
+                    console.log(res);
+                    $('.loginbox_login').click();
 
-    //         }
+
+                }
+
+
+            }
 
 
 
-    //     })
+        })
 
-    //     })
+    })
 
     $('.login').on('submit', function(e) {
         e.preventDefault()
         $.ajax({
-            method: "POST",
-            url: "http://ajax.frontend.itheima.net/api/login",
-            data: $(this).serialize(),
+            method: "post",
+            url: "http://api-breakingnews-web.itheima.net/api/login",
+            data: {
+                username: $('.login1').val(),
+                password: $('.login2').val(),
+            },
             success: function(res) {
                 console.log(res);
+                if (res.status !== 0) {
+                    layer.msg('登陆失败');
+                    location.href = "index.html"
+
+
+                } else {
+                    layer.msg('成功');
+
+                }
+
 
             }
         })
